3c765de463262e9475198f766f2193be1ecc18f1
Clément Rigo <rigoclement@mydnic.be>
2016-05-16 13:15:04 +0200

    add slug column

--- database/migrations/2016_05_15_171009_create_items_table.php
+++ database/migrations/2016_05_15_171009_create_items_table.php
@@ -15,6 +15,7 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
             $table->text('content');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
