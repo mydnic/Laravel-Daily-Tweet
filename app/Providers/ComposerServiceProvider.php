6096774e9d0f02352eeb7631cbadccbf664abd37
Clément Rigo <rigoclement@mydnic.be>
2016-05-20 22:12:07 +0200

    ugh

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -14,7 +14,7 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('layout.app', 'App\Http\ViewComposers\LayoutComposer');
+        View::composer('layouts.app', 'App\Http\ViewComposers\LayoutComposer');
     }
 
     /**
