8cab0077c888cd19890b5dd14457f24f00d9eac2
Clément Rigo <rigoclement@mydnic.be>
2016-05-20 10:32:11 +0200

    categories are sluggable

--- app/Category.php
+++ app/Category.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Sofa\Eloquence\Eloquence;
 
 class Category extends Model
 {
+    use SoftDeletes, SluggableTrait, Eloquence;
+
+    protected $sluggable = [
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    ];
+
     public function items()
     {
         return $this->belongsToMany('App\Item');
--- database/migrations/2016_05_16_110447_create_categories_table.php
+++ database/migrations/2016_05_16_110447_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->softDeletes();
             $table->timestamps();
         });
