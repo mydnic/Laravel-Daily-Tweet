bddfbd0303e81c3e5d3a0a9ba9b1b497665c8118
Clément Rigo <rigoclement@mydnic.be>
2016-11-03 21:50:51 +0100

    use homestead instead of vagrant

--- vagrant-install.sh
+++ vagrant-install.sh
@@ -16,7 +16,7 @@ echo -e "\n--- Install base packages ---\n"
 apt-get -y install vim curl build-essential python-software-properties git > /dev/null 2>&1
 
 echo -e "\n--- Add some repos to update our distro ---\n"
-add-apt-repository ppa:ondrej/php5 > /dev/null 2>&1
+add-apt-repository ppa:ondrej/php5-5.6 > /dev/null 2>&1
 add-apt-repository ppa:chris-lea/node.js > /dev/null 2>&1
 
 echo -e "\n--- Updating packages list ---\n"
@@ -37,7 +37,7 @@ mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
 mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
 
 echo -e "\n--- Installing PHP-specific packages ---\n"
-apt-get -y install php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql php-apc > /dev/null 2>&1
+apt-get -y install php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql php-apc php5-xsl php5-intl > /dev/null 2>&1
 
 echo -e "\n--- Enabling mod-rewrite ---\n"
 a2enmod rewrite > /dev/null 2>&1
