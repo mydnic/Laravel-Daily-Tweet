27ef779fd207c6e9484c3854b9178a4575cd9099
Clément Rigo <rigoclement@mydnic.be>
2020-12-03 16:19:37 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::get('{slug}', 'ItemController@show')->name('item.show');
 
 Route::prefix('admin')->as('admin.')->middleware('auth')->group(function () {
     Route::resource('item', 'Admin\ItemController');
-    Route::get('category/{id}/delete', 'Admin\CategoryController@delete')->name('admin.category.delete');
+    Route::get('category/{id}/delete', 'Admin\CategoryController@delete')->name('category.delete');
     Route::resource('category', 'Admin\CategoryController');
-    Route::get('setting', 'Admin\SettingController@edit')->name('admin.setting.edit');
-    Route::post('setting', 'Admin\SettingController@update')->name('admin.setting.update');
+    Route::get('setting', 'Admin\SettingController@edit')->name('setting.edit');
+    Route::post('setting', 'Admin\SettingController@update')->name('setting.update');
 });
